ced6ed8c887b7a8a521833e02de38eef0585d72a
Romullo Bernardo <romulloqueiroz@gmail.com>
2020-10-21 22:02:38 -0400

    switch finished

---
+++ src/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #59A0FF;
+  }
+`
---
+++ src/Switch.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+export default ({ checked = false, color ='#be2edd' }) => {
+  const [toggle, setToggle] = useState(checked)
+
+  return (
+    <Switch>
+      <Input type='checkbox' />
+      <Slider {...{ toggle, color }} onClick={() => setToggle(!toggle)} />
+    </Switch>
+  )
+}
+
+const Slider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0; 
+  bottom: 0;
+  background-color: ${({ toggle, color }) => toggle ? color : 'white'};
+  border-radius: 15px;
+  transition: 0.4s;
+
+  box-shadow: ${({ toggle }) => !toggle && '0px 0px 1px rgba(40, 41, 61, 0.04), 0px 2px 4px rgba(96, 97, 112, 0.16)'} ;
+
+  &:before {
+    content: '';
+
+    position: absolute;
+    left: 2px;
+    bottom: 2px;
+
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
+
+    background-color: ${({ toggle, color }) => toggle ? 'white' : color};
+    box-shadow: ${({ toggle }) => toggle && '0px 0px 1px rgba(40, 41, 61, 0.04), 0px 2px 4px rgba(96, 97, 112, 0.16)'} ;
+
+    transition: 0.4s;
+  }
+`
+
+const Input = styled.input`
+  &:checked + ${Slider} {
+    background-color: ${({ color }) => color};
+  }
+
+  &:checked + ${Slider}:before {
+    transform: translateX(23.4px);
+  } 
+`
+
+const Switch = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 48px;
+  height: 24px;
+
+  & ${Input} {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+`
